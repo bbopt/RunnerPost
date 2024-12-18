b4fcb2557ba2386f813d9e69c5f10738661e2acd
Christophe Tribes <christophe.tribes@polymtl.ca>
2024-12-18 13:58:04 -0500

    Version beta +

--- src/runnerpost_version.hpp
+++ src/runnerpost_version.hpp
@@ -1,8 +1,8 @@
 #undef RUNNERPOST
 
 /// Namespace alias RUNNERPOST is hard defined to distinguish between library versions
-#define RUNNERPOST RUNNERPOST_1_0_BETA3
+#define RUNNERPOST RUNNERPOST_1_0_BETA4
 
 /// Define version number
 #undef RUNNERPOST_VERSION_NUMBER
-#define RUNNERPOST_VERSION_NUMBER "1.0b3"
+#define RUNNERPOST_VERSION_NUMBER "1.0b4"
